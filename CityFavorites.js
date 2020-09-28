3c19668bb8d230537d7fe1fccdc6df54e8772bdc
Eric Vicenti <eric@aven.io>
2020-09-28 00:25:45 -0700

    implement store

--- CityFavorites.js
+++ CityFavorites.js
@@ -1,60 +1,42 @@
 import { useEffect, useState } from "react";
 
-const favorites = [];
+let favorites = [];
 const subscriptions = new Set();
 
-function notifyFavoriteChange() {
-  subscriptions.forEach((handle) => handle(favorites));
-}
-
-export function useCityFavorite(city) {
-  const initiallyIsFavorited = !!favorites.find(
-    (c) => c.cityId === city.cityId
-  );
-  const [isSubscribed, internalIsFavorite] = useState(initiallyIsFavorited);
-  useEffect(() => {
-    function handleFavoriteUpdate(newFavs) {
-      const isSubscribedNow = !!newFavs.find((c) => c.cityId === city.cityId);
-      if (isSubscribed !== isSubscribedNow) {
-        internalIsFavorite(isSubscribedNow);
-      }
-    }
-    subscriptions.add(handleFavoriteUpdate);
-    return () => {
-      subscriptions.delete(handleFavoriteUpdate);
-    };
-  }, [city.cityId]);
-  function setIsFavorite(isSubscribed) {
-    if (isSubscribed) {
-      const cityId = city.cityId;
-      const fav = favorites.find((c) => c.cityId === cityId);
-      if (!fav) {
-        favorites.push(city);
-        notifyFavoriteChange();
-      }
-    } else {
-      // remove city
-      const removalIndex = favorites.findIndex((c) => c.cityId === city.cityId);
-      if (removalIndex !== -1) {
-        favorites.splice(removalIndex, 1);
-        notifyFavoriteChange();
-      }
-    }
-  }
-
-  return [isSubscribed, setIsFavorite];
+function setFavorites(transaction) {
+  favorites = transaction(favorites);
+  subscriptions.forEach((handle) => {
+    handle(favorites);
+  });
 }
 
 export function useFavorites() {
-  const [internalFavorites, setInternalFavorites] = useState(favorites);
+  const [state, setState] = useState(favorites);
   useEffect(() => {
-    function handleUpdate(newFavs) {
-      setInternalFavorites(newFavs);
+    function handleUpdate(newFavorites) {
+      setState(newFavorites);
     }
     subscriptions.add(handleUpdate);
     return () => {
       subscriptions.delete(handleUpdate);
     };
   }, []);
-  return internalFavorites;
+  return state;
+}
+
+export function useCityFavorite(city) {
+  const favCities = useFavorites();
+  const isSubscribed = !!favCities.find(
+    (favCity) => favCity.cityId === city.cityId
+  );
+  function setIsFavorite(isFavorite) {
+    if (isFavorite) {
+      setFavorites((favorites) => [...favorites, city]);
+    } else {
+      setFavorites((favorites) =>
+        favorites.filter((c) => c.cityId !== city.cityId)
+      );
+    }
+  }
+  return [isSubscribed, setIsFavorite];
 }
